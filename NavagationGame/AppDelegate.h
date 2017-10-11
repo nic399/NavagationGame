ad12e7478e93d3f7ccba969b73e9c6752787a284
Nicholas Fung <nbfung@sfu.ca>
2017-10-10 19:27:45 -0700

    Initial Commit

---
+++ NavagationGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavagationGame
+//
+//  Created by Nicholas Fung on 2017-10-10.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavagationGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavagationGame
+//
+//  Created by Nicholas Fung on 2017-10-10.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
